7cdfa40de20ec65d662458ba0d0a263dc6852b09
Jean LEUNG <jean.leung@renault.com>
2020-04-23 18:23:46 +0200

    First working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyDYPspkruRTl2WSTR5Sgrj5I-p7IvMwVdE",
+      authDomain: "bibliotheque-d375a.firebaseapp.com",
+      databaseURL: "https://bibliotheque-d375a.firebaseio.com",
+      projectId: "bibliotheque-d375a",
+      storageBucket: "bibliotheque-d375a.appspot.com",
+      messagingSenderId: "969810712342",
+      appId: "1:969810712342:web:873c3adc0009c074a02237"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
